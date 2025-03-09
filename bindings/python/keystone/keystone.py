6044c97b9a8ffa89b57402811c99c4e68b4f52e5
Antelox <anteloxrce@gmail.com>
2025-03-09 18:45:11 +0100

    Bindings Python: Porting

--- bindings/python/keystone/keystone.py
+++ bindings/python/keystone/keystone.py
@@ -1,85 +1,63 @@
 # Keystone Python bindings, by Nguyen Anh Quynnh <aquynh@gmail.com>
-import os.path
+import os
 import sys
-_python2 = sys.version_info[0] < 3
-if _python2:
+import sysconfig
+from .keystone_const import *
+from ctypes import *
+from os.path import join
+
+__version__ = "%u.%u.%u" % (KS_VERSION_MAJOR, KS_VERSION_MINOR, KS_VERSION_EXTRA)
+
+if sys.version_info[0] < 3:
     range = xrange
 
-from . import arm_const, arm64_const, mips_const, sparc_const, hexagon_const, ppc_const, systemz_const, x86_const, evm_const
-from .keystone_const import *
+if sys.version_info >= (3, 9):
+    import importlib.resources as resources
+else:
+    import importlib_resources as resources
 
-from ctypes import *
-from platform import system
-from os.path import split, join, dirname, exists
-import sys
+if sys.platform == 'darwin':
+    _lib = "libkeystone.dylib"
+elif sys.platform == 'win32':
+    _lib = "keystone.dll"
+elif sys.platform == 'cygwin':
+    _lib = "cygkeystone-0.dll"
+else:
+    _lib = "libkeystone.so"
+
+_ks = None
 
 
-import inspect
-if not hasattr(sys.modules[__name__], '__file__'):
-    __file__ = inspect.getfile(inspect.currentframe())
+def _load_lib(path):
+    lib_file = join(path, _lib)
+    if os.path.exists(lib_file):
+        return cdll.LoadLibrary(lib_file)
 
-_lib_path = os.path.join(split(__file__)[0], 'lib')
-_all_libs = ('keystone.dll', 'libkeystone.so', 'libkeystone.so.%u' %KS_API_MAJOR, 'libkeystone.dylib')
-_found = False
 
-for _lib in _all_libs:
-    try:
-        _lib_file = join(_lib_path, _lib)
-        #print(">> 0: Trying to load %s" %_lib_file);
-        _ks = cdll.LoadLibrary(_lib_file)
-        _found = True
-        break
-    except OSError:
-        pass
+# Loading attempts, in order
+# - user-provided environment variable
+# - importlib.resources can get us the path to the local libraries
+# - python's lib directory
+# - last-gasp attempt at some hardcoded paths on darwin and linux
 
-if _found == False:
-    # try loading from default paths
-    for _lib in _all_libs:
-        try:
-            #print(">> 1: Trying to load %s" %_lib);
-            _ks = cdll.LoadLibrary(_lib)
-            _found = True
-            break
-        except OSError:
-            pass
+_path_list = [os.getenv('LIBKEYSTONE_PATH', None),
+              str(resources.files('keystone') / 'lib'),
+              sysconfig.get_path('platlib'),
+              "/usr/local/lib/" if sys.platform == 'darwin' else '/usr/lib64']
 
-if _found == False:
-    # last try: loading from python lib directory
-    import distutils.sysconfig
-    _lib_path = distutils.sysconfig.get_python_lib()
-    for _lib in _all_libs:
-        try:
-            _lib_file = join(_lib_path, 'keystone', _lib)
-            #print(">> 2: Trying to load %s" %_lib_file);
-            _ks = cdll.LoadLibrary(_lib_file)
-            _found = True
-            break
-        except OSError:
-            pass
-
-# Attempt Linux/Darwin specific load (10.11 specific),
-# since LD_LIBRARY_PATH is not guaranteed to exist
-if (_found == False) and (system() != 'Windows'):
-    _lib_path = '/usr/local/lib/'
-    for _lib in _all_libs:
-        try:
-            _lib_file = join(_lib_path, _lib)
-            #print(">> 3: Trying to load %s" %_lib_file);
-            _ks = cdll.LoadLibrary(_lib_file)
-            _found = True
-            break
-        except OSError:
-            pass
-
-if _found == False:
+for _path in _path_list:
+    if _path is None: continue
+    _ks = _load_lib(_path)
+    if _ks is not None: break
+else:
     raise ImportError("ERROR: fail to load the dynamic library.")
 
-__version__ = "%u.%u.%u" %(KS_VERSION_MAJOR, KS_VERSION_MINOR, KS_VERSION_EXTRA)
 
 # setup all the function prototype
 def _setup_prototype(lib, fname, restype, *argtypes):
     getattr(lib, fname).restype = restype
     getattr(lib, fname).argtypes = argtypes
+
 
 kserr = c_int
 ks_engine = c_void_p
@@ -92,11 +70,13 @@ _setup_prototype(_ks, "ks_close", kserr, ks_engine)
 _setup_prototype(_ks, "ks_strerror", c_char_p, kserr)
 _setup_prototype(_ks, "ks_errno", kserr, ks_engine)
 _setup_prototype(_ks, "ks_option", kserr, ks_engine, c_int, c_void_p)
-_setup_prototype(_ks, "ks_asm", c_int, ks_engine, c_char_p, c_uint64, POINTER(POINTER(c_ubyte)), POINTER(c_size_t), POINTER(c_size_t))
+_setup_prototype(_ks, "ks_asm", c_int, ks_engine, c_char_p, c_uint64, POINTER(POINTER(c_ubyte)), POINTER(c_size_t),
+                 POINTER(c_size_t))
 _setup_prototype(_ks, "ks_free", None, POINTER(c_ubyte))
 
 # callback for OPT_SYM_RESOLVER option
 KS_SYM_RESOLVER = CFUNCTYPE(c_bool, c_char_p, POINTER(c_uint64))
+
 
 # access to error code via @errno of KsError
 # this also includes the @stat_count returned by ks_asm
@@ -121,12 +101,12 @@ def ks_version():
     major = c_int()
     minor = c_int()
     combined = _ks.ks_version(byref(major), byref(minor))
-    return (major.value, minor.value, combined)
+    return major.value, minor.value, combined
 
 
 # return the binding's version
 def version_bind():
-    return (KS_API_MAJOR, KS_API_MINOR, (KS_API_MAJOR << 8) + KS_API_MINOR)
+    return KS_API_MAJOR, KS_API_MINOR, (KS_API_MAJOR << 8) + KS_API_MINOR
 
 
 # check to see if this engine supports a particular arch
@@ -156,7 +136,6 @@ class Ks(object):
         else:
             self._syntax = None
 
-
     # destructor to be called automatically when object is destroyed.
     def __del__(self):
         if self._ksh:
@@ -165,15 +144,13 @@ class Ks(object):
                 self._ksh = None
                 if status != KS_ERR_OK:
                     raise KsError(status)
-            except: # _ks might be pulled from under our feet
+            except:  # _ks might be pulled from under our feet
                 pass
-
 
     # return assembly syntax.
     @property
     def syntax(self):
         return self._syntax
-
 
     # syntax setter: modify assembly syntax.
     @syntax.setter
@@ -184,11 +161,9 @@ class Ks(object):
         # save syntax
         self._syntax = style
 
-
     @property
     def sym_resolver(self):
         return
-
 
     @sym_resolver.setter
     def sym_resolver(self, resolver):
@@ -199,7 +174,6 @@ class Ks(object):
         # save resolver
         self._sym_resolver = callback
 
-
     # assemble a string of assembly
     def asm(self, string, addr=0, as_bytes=False):
         encode = POINTER(c_ubyte)()
@@ -209,12 +183,12 @@ class Ks(object):
             string = string.encode('ascii')
 
         status = _ks.ks_asm(self._ksh, string, addr, byref(encode), byref(encode_size), byref(stat_count))
-        if (status != 0):
+        if status != 0:
             errno = _ks.ks_errno(self._ksh)
             raise KsError(errno, stat_count.value)
         else:
             if stat_count.value == 0:
-                return (None, 0)
+                return None, 0
             else:
                 if as_bytes:
                     encoding = string_at(encode, encode_size.value)
@@ -224,15 +198,15 @@ class Ks(object):
                         encoding.append(encode[i])
 
                 _ks.ks_free(encode)
-                return (encoding, stat_count.value)
+                return encoding, stat_count.value
 
 
 # print out debugging info
 def debug():
-    archs = { "arm": KS_ARCH_ARM, "arm64": KS_ARCH_ARM64, \
-        "mips": KS_ARCH_MIPS, "sparc": KS_ARCH_SPARC, \
-        "systemz": KS_ARCH_SYSTEMZ, "ppc": KS_ARCH_PPC, \
-        "hexagon": KS_ARCH_HEXAGON, "x86": KS_ARCH_X86, 'evm': KS_ARCH_EVM }
+    archs = {"arm": KS_ARCH_ARM, "arm64": KS_ARCH_ARM64,
+             "mips": KS_ARCH_MIPS, "sparc": KS_ARCH_SPARC,
+             "systemz": KS_ARCH_SYSTEMZ, "ppc": KS_ARCH_PPC,
+             "hexagon": KS_ARCH_HEXAGON, "x86": KS_ARCH_X86, 'evm': KS_ARCH_EVM}
 
     all_archs = ""
     keys = archs.keys()
@@ -243,4 +217,3 @@ def debug():
     (major, minor, _combined) = ks_version()
 
     return "python-%s-c%u.%u-b%u.%u" % (all_archs, major, minor, KS_API_MAJOR, KS_API_MINOR)
-
